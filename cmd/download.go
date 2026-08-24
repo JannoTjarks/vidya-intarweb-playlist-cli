c10188468af81b66ffb2d2f9c0f9fcffb4689e39
Janno Tjarks <janno.tjarks@mailbox.org>
2026-08-24 07:31:53 +0200

    chore: move destinationPath validation to the Cobra command PreRunE

--- cmd/download.go
+++ cmd/download.go
@@ -14,7 +14,25 @@ var downloadCmd = &cobra.Command{
 	Use:   "download",
 	Short: "Downloads tracks from VIP servers",
 	Long:  "Download the tracks from the VIP servers of the chosen roster. The files are downloaded as .m4a files and will also get tagged with some metadata, e.g. Title and Album/Game.",
-	RunE: func(cmd *cobra.Command, args []string) error {
+	Run: func(cmd *cobra.Command, args []string) {
+		utils.DownloadTracksByRoster(
+			viper.GetString(rosterName.name),
+			viper.GetString(destinationPath.name),
+		)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(downloadCmd)
+	downloadCmd.Flags().String(rosterName.name, rosterName.value, rosterName.usage)
+	downloadCmd.Flags().String(destinationPath.name, destinationPath.value, destinationPath.usage)
+
+	downloadCmd.PreRunE = func(cmd *cobra.Command, args []string) error {
+		viper.BindPFlags(cmd.Flags())
+		viper.AutomaticEnv()
+
+		viper.BindEnv(rosterName.name, rosterName.envVar)
+
 		fileStat, err := os.Stat(viper.GetString(destinationPath.name))
 		if err != nil {
 			return err
@@ -24,24 +42,7 @@ var downloadCmd = &cobra.Command{
 			return errors.New("the given destinationPath has to be a directory")
 		}
 
-		utils.DownloadTracksByRoster(
-			viper.GetString(rosterName.name),
-			viper.GetString(destinationPath.name),
-		)
-
 		return nil
-	},
-}
-
-func init() {
-	rootCmd.AddCommand(downloadCmd)
-	downloadCmd.Flags().String(rosterName.name, rosterName.value, rosterName.usage)
-	downloadCmd.Flags().String(destinationPath.name, destinationPath.value, destinationPath.usage)
-
-	downloadCmd.PreRun = func(cmd *cobra.Command, args []string) {
-		viper.BindPFlags(cmd.Flags())
-		viper.AutomaticEnv()
-
-		viper.BindEnv(rosterName.name, rosterName.envVar)
 	}
+
 }
